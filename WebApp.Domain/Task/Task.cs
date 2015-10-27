24e1d151b1e220ceff6c5c49981264b3d46c74b3
maxrus1982 <maxrus1982@rambler.ru>
2015-10-27 22:58:11 +0300

    Domain: Task

--- WebApp.Domain/Task/Task.cs
+++ WebApp.Domain/Task/Task.cs
@@ -6,7 +6,12 @@ namespace WebApp.Domain
 {
     public class Task : IDocument
     {
-        public Guid ID { get; set; }
-        public String Name { get; set; }
+        public virtual Guid ID { get; set; }
+        public virtual String User { get; set; }
+        public virtual String Name { get; set; }
+        public virtual DateTime CreateDate { get; set; }
+        public virtual DateTime? BeginDate { get; set; }
+        public virtual DateTime? EndDate { get; set; }
+        public virtual Boolean IsCompleted { get; set; }
     }
 }
