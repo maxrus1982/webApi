e7a799d2e902601f584790ceba19b8ae52b26e41
maxrus1982 <maxrus1982@rambler.ru>
2015-10-29 20:51:39 +0300

    commit fix

---
+++ WebApp/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WebApp.Core;
+
+namespace WebApp
+{
+    public class UserContext : IUserContext
+    {
+        public String UserID { get; set; }
+    }
+}
