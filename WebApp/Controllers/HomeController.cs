bc8875ee513bb36ddf8b1083bf7a84f51fa48ac7
Ruslan.Kadyrbaev <maxrus1982@rambler.ru>
2015-11-04 00:40:47 +0300

    init data

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.Practices.Unity;
 using System.Web.Mvc;
+
+using WebApp.Service;
+using WebApp.Service.Interface;
+using WebApp.Service.TaskRequests;
 
 namespace WebApp.Controllers
 {
@@ -10,6 +15,17 @@ namespace WebApp.Controllers
     {
         public ActionResult Index()
         {
+            ///этот код инициализации должен быть ну максмимум в сиде/миграции, а вообще он не нужен
+            ///а здесь чисто для демо
+            var __rep = new TaskRepository();
+            __rep.UserContext = new UserContext();
+            __rep.UserContext.UserID = "Foo";
+            __rep.DbContext = WebApp.Core.IoC.Container.Resolve<ITaskContext>();
+            if (__rep.GetList(new TaskRequest()).Select(x => x.ID).Count() == 0)
+            {
+                AddTestRows(__rep);
+            }
+
             return View();
         }        
 
@@ -17,6 +33,27 @@ namespace WebApp.Controllers
         {
             //db.Dispose();
             base.Dispose(disposing);
+        }
+
+        private void AddTestRows(TaskRepository repository)
+        {
+            for (int i = 1; i <= 20; i++)
+                AddTask("Foo_" + i, true, repository);
+            for (int i = 1; i <= 20; i++)
+                AddTask("Foo_" + i, false, repository);
+        }
+
+        private void AddTask(string name, bool completed, TaskRepository repository)
+        {
+            var __id = Guid.NewGuid();
+            var __documentDTO = repository.New(new CreateTaskRequest()
+            {
+                PlanBeginDate = DateTime.Now,
+                PlanEndDate = DateTime.Now.AddDays(100)
+            });
+            __documentDTO.Name = name;
+            __documentDTO.Completed = completed;
+            __documentDTO = repository.Post(__documentDTO);
         }
     }
 }
